dc7f0f78426997d1016091938b96bcb4cf47f68b
Vyan <thevyanmadai@gmail.com>
2025-12-26 20:00:50 +0545

    post

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-import qrcode
+from PyPDF2 import PdfMerger 
+import os                   
 
-data = input("Enter data to encode in QR: ")
+def merge_pdfs(folder_path, output_name):
+    # Create a PDF merger object
+    merger = PdfMerger()
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction= qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
+    # Get all PDF files from the given folder
+    files = [f for f in os.listdir(folder_path) if f.endswith(".pdf")]
 
-qr.add_data(data)
-qr.make(fit=True)
+    # Sort files alphabetically
+    files.sort()
 
-img = qr.make_image(fill_color = "black",back_color = "white")
-img.save("qr_code.png")
+    # Add each PDF file into the merger
+    for pdf in files:
+        path = os.path.join(folder_path, pdf)  
+        merger.append(path)                  
+        print("Added:", pdf)
 
-print("QR code saved as 'qr_code.png'.")
+    # Create output file path
+    output_path = os.path.join(folder_path, output_name)
+
+    # Write all merged PDFs into a single file
+    merger.write(output_path)
+    merger.close() 
+
+    print(f"\nAll PDFs merged successfully into: {output_name}")
+
+if __name__ == "__main__":
+    print("===== PDF Merger Tool =====")
+
+    #Ask the user for folder path and output name
+    folder = input("Enter folder path: ").strip()
+    output = input("Enter output file name: ").strip()
+
+    try:
+        merge_pdfs(folder, output)
+    except Exception as e:
+        print(f"Error Occurred: {e}") 
